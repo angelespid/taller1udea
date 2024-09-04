c250c2c4cb8d8a5c61ae7e8764cbf9c0d05b256b
angelespid <angelespid@gmail.com>
2024-09-03 20:38:12 -0500

    Mi primer commit

---
+++ Taller/hola_mundo.cpp
@@ -0,0 +1,6 @@
+#include <iostream>  // Incluye la librería estándar para la entrada y salida
+
+int main() {
+    std::cout << "Hola, mundo!" << std::endl;  // Imprime el mensaje en la consola
+    return 0;  // Indica que el programa terminó correctamente
+}
